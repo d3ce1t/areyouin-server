86774f8a04ed154d46f7548024769588cb00e159
Jose Padilla <sargepl@gmail.com>
2016-05-05 20:15:57 +0200

    Add prefix to Protocol generated classes

--- common/proto/generate.sh
+++ common/proto/generate.sh
@@ -2,4 +2,4 @@
 protoc --go_out=../ core.proto
 protoc --java_out=/Users/jpadilla/AndroidStudioProjects/AreYouIN/app/src/main/java core.proto
 #protoc --swift_out="/Users/jpadilla/Git/AreYouIN/areyouin-ios/AreYouIN/" core.proto
-protoc --objc_out="/Users/jpadilla/Git/AreYouIN/areyouin-ios/AreYouIN/" core.proto
+protoc --objc_out="/Users/jpadilla/Git/Peeple/areyouin-ios/AreYouIN/" core.proto
--- protocol/proto/generate.sh
+++ protocol/proto/generate.sh
@@ -2,4 +2,4 @@
 protoc -I../../common/proto/ -I./ --go_out=Mcore.proto=peeple/areyouin/common:../ protocol.proto
 protoc -I../../common/proto/ -I./ --java_out=/Users/jpadilla/AndroidStudioProjects/AreYouIN/app/src/main/java protocol.proto
 #protoc -I../../common/proto/ -I./ --swift_out="/Users/jpadilla/Git/AreYouIN/areyouin-ios/AreYouIN/" protocol.proto
-protoc -I../../common/proto/ -I./ --objc_out="/Users/jpadilla/Git/AreYouIN/areyouin-ios/AreYouIN/" protocol.proto
+protoc -I../../common/proto/ -I./ --objc_out="/Users/jpadilla/Git/Peeple/areyouin-ios/AreYouIN/" protocol.proto
